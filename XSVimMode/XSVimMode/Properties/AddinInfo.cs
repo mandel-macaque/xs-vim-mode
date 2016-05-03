1ac125d46ea6fc5691e1f56686de4fc9c0e36306
Manuel de la Pena <mandel@themacaque.com>
2016-05-03 10:35:41 +0200

    Create an addins project to extend XS.

---
+++ XSVimMode/XSVimMode/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin (
+	"XSVimMode",
+	Namespace = "XSVimMode",
+	Version = "1.0"
+)]
+
+[assembly: AddinName ("XSVimMode")]
+[assembly: AddinCategory ("IDE extensions")]
+[assembly: AddinDescription ("XSVimMode")]
+[assembly: AddinAuthor ("mandel")]
+
